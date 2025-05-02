ed0a7a384919f71d52d4899cba89f684a306e7dc
Makaroshka1234 <makstutik007@gmail.com>
2025-05-02 17:09:21 +0300

    sdsd

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,15 @@ import { getFirestore } from "firebase/firestore";
 
 
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyDHfx5XFa1jOdoy6nzKodu8aTkEniW5_D8",
-        authDomain: "todo-test-b4383.firebaseapp.com",
-        projectId: "todo-test-b4383",
-        storageBucket: "todo-test-b4383.firebasestorage.app",
-        messagingSenderId: "789747928355",
-        appId: "1:789747928355:web:071d544e34ee3ec3b19096",
-        measurementId: "G-MJ105RF8F9"
-      };
+const firebaseConfig = {
+    apiKey: "AIzaSyDHfx5XFa1jOdoy6nzKodu8aTkEniW5_D8",
+    authDomain: "todo-test-b4383.firebaseapp.com",
+    projectId: "todo-test-b4383",
+    storageBucket: "todo-test-b4383.firebasestorage.app",
+    messagingSenderId: "789747928355",
+    appId: "1:789747928355:web:071d544e34ee3ec3b19096",
+    measurementId: "G-MJ105RF8F9"
+  };
 
 
 console.log(process.env)
